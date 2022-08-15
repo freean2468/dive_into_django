2f9336b05e1f8953ccc777b2c0f9e2084fc5625e
sensebecommon <freean2468@gmail.com>
2022-08-15 23:52:32 +0900

    feat : admin 페이지를 위해 UserAdmin 커스텀

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
 from typing import Optional, Sequence
+from django.utils.translation import gettext, gettext_lazy as _
 
 # Register your models here.
 
@@ -9,6 +9,23 @@ from . import models
 
 class UserAdmin(BaseUserAdmin):
     ordering: Optional[Sequence[str]] = ('email', )
+    fieldsets = (
+        (None, {'fields': ('nickname', 'password')}),
+        (_('Personal info'), {'fields': ('name', 'phone', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'phone', 'nickname', 'name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email', 'phone', 'nickname', 'email')
 
 
 # admin.site.unregister(User)
